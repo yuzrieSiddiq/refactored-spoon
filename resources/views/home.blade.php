3290a2e0bb5ee01d8dcb0b6bd104fa837d31c791
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-03-29 10:42:51 +0800

    [ADD] papa parse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,25 +27,50 @@
 @endsection
 
 @section('extra_js')
+<script src="{{ asset('js/papaparse.min.js') }}"></script>
 <script>
 (function() {
+
+    let csvdata = '';
+    let setCSVdata = function(jsonstring) {
+        csvdata = jsonstring
+    }
+
+    let getCSVdata = function() {
+        return csvdata
+    }
+
     let getToken = function() {
         return $('meta[name=csrf-token]').attr('content')
     }
 
-    $('.process').click(function() {
-        let data = {
-            '_token': getToken(),
-            'file': $('#excel').val()
-        }
-        $.ajax({
-            'url': '{{ route('excel.import') }}',
-            'method': 'POST',
-            'data': data,
-            'enctype': 'multipart/form-data'
-        }).done(function(response) {
-            console.log(response)
+    let papaparseafile = function(file) {
+        let jsonstring = ''
+        Papa.parse(file[0].files[0], {
+            complete: function(results) {
+                jsonstring = JSON.stringify(results.data)
+            }
         })
+        return jsonstring
+    }
+
+    $('.process').click(function() {
+        let csvparse = papaparseafile($('#excel'))
+
+        console.log(csvparse)
+
+        // let data = {
+        //     '_token': getToken(),
+        //     'file': $('#excel').val()
+        // }
+        // $.ajax({
+        //     'url': '{{ route('excel.import') }}',
+        //     'method': 'POST',
+        //     'data': data,
+        //     'enctype': 'multipart/form-data'
+        // }).done(function(response) {
+        //     console.log(response)
+        // })
     })
 })()
 </script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+const papaparsePath = './node_modules/papaparse/'
+mix.copy(papaparsePath + 'papaparse.min.js', 'public/js/papaparse.min.js');
