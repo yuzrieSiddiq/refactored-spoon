24f76796587139b992e4adfd9574e4d8d4f32239
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-04-02 15:52:36 +0800

    [ADD] laravel-permissions [REMOVE] excel-maatwebsite

--- config/app.php
+++ config/app.php
@@ -170,7 +170,7 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Dingo\Api\Provider\LaravelServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
