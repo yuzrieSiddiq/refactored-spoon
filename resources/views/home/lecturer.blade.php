3407667d47b264ff802a431a87733161f320bb80
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-05-02 19:12:37 +0800

    [UPDATE] remove menu and use tabs instead

--- resources/views/home/lecturer.blade.php
+++ resources/views/home/lecturer.blade.php
@@ -3,11 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
-            @include('layouts.menu_side')
-        </div>
         {{-- MAIN PANEL --}}
-        <div class="col-md-9">
+        <div class="col-md-12">
 
             <div class="panel panel-default">
                 <div class="panel-heading">
--- resources/views/unit/show.blade.php
+++ resources/views/unit/show.blade.php
@@ -7,10 +7,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
-            @include('layouts.menu_side')
-        </div>
-        <div class="col-md-9">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div> {{-- tabs --}}
