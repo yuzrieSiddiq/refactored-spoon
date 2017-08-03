f7de0e4f32662f8fb02ff46b8001ef43b8a322c3
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-08-03 16:04:23 +0800

    [CHANGE] when settings change copy over the existing quizzes

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\Settings;
+use App\Model\Quiz;
 
 class SettingsController extends Controller
 {
@@ -13,10 +14,29 @@ class SettingsController extends Controller
 
         // fetch the correct settings and update the correct fields
         $semester = Settings::where('name', 'semester')->first();
-        $semester->update([ 'value' => $input['settings-semester'] ]);
-
         $year = Settings::where('name', 'year')->first();
+
+        // get the quizzes
+        $quizzes = Quiz::with('questions')
+            ->where('semester', $semester->value)
+            ->where('year', $year->value)
+            ->get();
+
+        // update the settings
+        $semester->update([ 'value' => $input['settings-semester'] ]);
         $year->update([ 'value' => $input['settings-year'] ]);
 
+        /** Copy over the quizzes to the next semester if exist **/
+        foreach ($quizzes as $quiz) {
+            Quiz::create([
+                'unit_id'           => $quiz->unit_id,
+                'title'             => $quiz->title ,
+                'type'              => $quiz->type,
+                'semester'          => $semester->value,
+                'year'              => $year->value,
+                'show_questions'    => $quiz->show_questions,
+                'individual_only'   => $quiz->individual_only,
+            ]);
+        }
     }
 }
